4451be75192116a2805350681fe25e85288e1917
sachidanandgiri <sachidanandgiri23@gmail.com>
2020-01-27 09:23:51 +0530

    Functional and Class componenets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
+import Sidebar from './components/SidebarComponent';
+import Contact from './components/Contact'
+import About from './components/About'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
+        <Sidebar />
+        <Contact />
+        <About />
+        <h5>Hello Sachin</h5>
+        <h6>Hello Mania</h6>  
       </header>
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h4',
+            {id: 'name', className: 'name'},
+            'Hello Pawan'
+        )
+    )
+}
+
+export default About
